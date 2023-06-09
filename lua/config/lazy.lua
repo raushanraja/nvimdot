56570ff9c373e2847e80858dfd08d2244455e4aa
raushanraja <vraushanraja@gmail.com>
2023-06-10 03:21:20 +0530

    add example disabled plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -3,24 +3,58 @@
 --    `:help lazy.nvim.txt` for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  }
+    vim.fn.system {
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable', -- latest stable release
+        lazypath,
+    }
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  spec = {
-    { import = "plugins" },
-  },
-  defaults = {
-    lazy = false,
-    version = false, 
-  },
-  checker = { enabled = true }, -- automatically check for plugin updates
+    spec = {
+        { import = "plugins" },
+    },
+    defaults = {
+        lazy = false,
+        version = false,
+    },
+    checker = { enabled = true }, -- automatically check for plugin updates
+    performance = {
+        rtp = {
+            disabled_plugins = {
+            --    "2html_plugin",
+            --    "tohtml",
+            --    "getscript",
+            --    "getscriptPlugin",
+            --    "gzip",
+            --    "logipat",
+            --    "netrw",
+            --    "netrwPlugin",
+            --    "netrwSettings",
+            --    "netrwFileHandlers",
+            --    "matchit",
+            --    "tar",
+            --    "tarPlugin",
+            --    "rrhelper",
+            --    "spellfile_plugin",
+            --    "vimball",
+            --    "vimballPlugin",
+            --    "zip",
+            --    "zipPlugin",
+            --    "tutor",
+            --    "rplugin",
+            --    "syntax",
+            --    "synmenu",
+            --    "optwin",
+            --    "compiler",
+            --    "bugreport",
+            --    "ftplugin",
+            --    "editorconfig",
+            },
+        },
+    },
 })
