56b54b23b8cbb263f23081a80a14bd9f9b986534
raushanraja <vraushanraja@gmail.com>
2023-06-11 13:58:55 +0530

    add python lint

--- after/plugin/formatter_nullls.lua
+++ after/plugin/formatter_nullls.lua
@@ -4,5 +4,7 @@ null_ls.setup({
     sources = {
         null_ls.builtins.formatting.autopep8,
         null_ls.builtins.formatting.rustfmt,
+        null_ls.builtins.diagnostics.mypy,
+        null_ls.builtins.diagnostics.ruff,
     },
 })
