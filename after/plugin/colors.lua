8a278eaa7a86411a4e93fb2937a3ca77ce9b57da
raushanraja <vraushanraja@gmail.com>
2023-06-11 16:27:45 +0530

    update background

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -3,7 +3,9 @@ function Colors(color)
     vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+    vim.api.nvim_set_hl(0, "NormalNC", {bg = "none"})
     vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+    vim.api.nvim_set_hl(0,"TelescopeNormal",{bg="none"})
 end
 
 Colors()
